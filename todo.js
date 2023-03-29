975ea34b24e3789cd58ddafa1af21a8610c52250
Benjamin <benadegbaju@gmail.com>
2023-03-29 11:56:47 +0100

    JS file added, array list of Todos

---
+++ todo.js
@@ -0,0 +1,7 @@
+console.log('This JS script is loaded');
+
+const todoList = [
+  { id: 0, title: 'Do something', description: 'An example of something to do', done: false },
+  { id: 1, title: 'Do another thing', description: 'An example of another ToDo', done: false },
+  { id: 2, title: 'Something done', description: 'This item has been done', done: true }
+];
